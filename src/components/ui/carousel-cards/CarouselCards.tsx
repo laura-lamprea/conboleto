3d943fa46aeb22e2d7ccd4fb3e461c7f193784eb
laura.lamprea <laura.lamprea@upbirk.com>
2024-01-23 12:28:30 -0500

    CB-01 Footer, add figma icons

--- src/components/ui/carousel-cards/CarouselCards.tsx
+++ src/components/ui/carousel-cards/CarouselCards.tsx
@@ -6,24 +6,13 @@ import "../../../app/globals.css";
 import Card from "../../ui/card/Card";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { IconArrowNext, IconArrowPrev } from "@/assets/figmaIcons";
 
 function PrevArrow(props: any) {
 	const { className, style, onClick } = props;
 	return (
-		<div className={className} style={{ ...style }} onClick={onClick}>
-			<svg
-				width="42"
-				height="60"
-				viewBox="0 0 42 60"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					id="Vector"
-					d="M1.40715 27.2744L28.5987 2.13636C30.4781 0.39888 33.5171 0.39888 35.3765 2.13636L39.8951 6.31371C41.7745 8.05119 41.7745 10.8607 39.8951 12.5797L20.6212 30.3981L39.8951 48.2166C41.7745 49.954 41.7745 52.7636 39.8951 54.4826L35.3965 58.6969C33.5171 60.4344 30.4781 60.4344 28.6187 58.6969L1.42715 33.5589C-0.472256 31.8214 -0.47226 29.0119 1.40715 27.2744Z"
-					fill="#727272"
-				/>
-			</svg>
+		<div className={className} style={style} onClick={onClick}>
+			<IconArrowPrev fillColor={"#727272"} />
 		</div>
 	);
 }
@@ -32,19 +21,7 @@ function NextArrow(props: any) {
 	const { className, style, onClick } = props;
 	return (
 		<div className={className} style={{ ...style }} onClick={onClick}>
-			<svg
-				width="42"
-				height="60"
-				viewBox="0 0 42 60"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					id="Vector"
-					d="M39.8975 32.7256L12.706 57.8636C10.8266 59.6011 7.78757 59.6011 5.92815 57.8636L1.40956 53.6863C-0.469853 51.9488 -0.469853 49.1393 1.40956 47.4203L20.6835 29.6019L1.40956 11.7834C-0.469853 10.046 -0.469853 7.23642 1.40956 5.51743L5.90815 1.30311C7.78757 -0.43437 10.8266 -0.43437 12.686 1.30311L39.8775 26.4411C41.7769 28.1786 41.7769 30.9881 39.8975 32.7256Z"
-					fill="#727272"
-				/>
-			</svg>
+			<IconArrowNext fillColor={"#727272"} />
 		</div>
 	);
 }
@@ -53,50 +30,50 @@ export const CarouselCards = ({ data }: { data: any }) => {
 	const settings = {
 		infinite: true,
 		autoplay: true,
+		centerPadding: "700px",
 		slidesToShow: 4,
-		slidesToScroll: 3,
+		// slidesToScroll: 3,
 		speed: 1000,
 		autoplaySpeed: 2500,
 		cssEase: "linear",
 		responsive: [
 			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 4,
-					slidesToScroll: 3,
-					infinite: true,
-					dots: true,
-				},
-			},
-			{
-				breakpoint: 600,
+				breakpoint: 1696,
 				settings: {
 					slidesToShow: 3,
 					slidesToScroll: 2,
-					initialSlide: 2,
+					infinite: true,
 				},
 			},
 			{
-				breakpoint: 480,
+				breakpoint: 1300,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 1020,
 				settings: {
 					slidesToShow: 1,
 					slidesToScroll: 1,
 				},
 			},
 		],
-		prevArrow: <PrevArrow className="h-11 w-11" />,
-		nextArrow: <NextArrow className="h-11 w-11" />,
+		prevArrow: <PrevArrow />,
+		nextArrow: <NextArrow />,
 	};
 	return (
-		<div className="px-20">
-			<Slider {...settings} className="px-10 justify-between">
-				{data.map((item: any, index: number) => (
-					<Link href={`/`} key={index}>
-						<Card event={item}  />
-					</Link>
-				))}
-			</Slider>
-		</div>
+		<Slider
+			{...settings}
+			className="ml-16 mr-24 xl:pl-[calc(5%)] lg:pl-[calc(10%)] md:pl-[calc(23%)] sm:pl-[calc(12%)]"
+		>
+			{data.map((item: any, index: number) => (
+				<Link href={`/`} key={index}>
+					<Card event={item} />
+				</Link>
+			))}
+		</Slider>
 	);
 };
 
--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -1,69 +1,103 @@
+import Link from "next/link";
+import { Roboto } from "next/font/google";
+import Image from "next/image";
+import logo from "@/assets/images/logo.png";
+import { FaLinkedinIn, FaFacebookF } from "react-icons/fa";
+const roboto100 = Roboto({ weight: "100", subsets: ["latin"] });
+const roboto400 = Roboto({ weight: "400", subsets: ["latin"] });
+
+const siteMap = [
+	{
+		title: "INFORMACIÓN",
+		items: [
+			{ title: "Inicio", link: "/" },
+			{ title: "Acerca de nosotros", link: "/" },
+			{ title: "Crea tu evento", link: "/" },
+			{ title: "Blog", link: "/" },
+		],
+	},
+	{
+		title: "EVENTOS",
+		items: [
+			{ title: "Conciertos", link: "/" },
+			{ title: "Deportes", link: "/" },
+			{ title: "Eventos culturales", link: "/" },
+			{ title: "Standup", link: "/" },
+			{ title: "Teatro", link: "/" },
+		],
+	},
+	{
+		title: "LEGAL",
+		items: [
+			{ title: "Términos y condiciones", link: "/" },
+			{ title: "Aviso de privacidad", link: "/" },
+			{ title: "Términos de compra", link: "/" },
+		],
+	},
+	{
+		title: "SOPORTE Y CONTACTO",
+		items: [
+			{ title: "soporte@conboleto.co", link: "/" },
+			{ title: "+55 2399 1145", link: "/" },
+			{ title: "Contáctanos", link: "/" },
+			{ title: "Facebook", link: "/" },
+			{ title: "Linkedin", link: "/" },
+		],
+	},
+];
+
 const Footer = () => {
 	return (
 		<footer className="footer flex flex-col items-center w-full h-[744px] bg-slate-900">
 			<div className="pt-16">
-				<span className="text-white text-[32px] font-extrabold">CREA </span>
+				<span className="text-[32px] font-extrabold">CREA </span>
 				<span className="text-rose-600 text-[32px] font-extrabold">
 					TU EVENTO
 				</span>
-				<span className="text-white text-[32px] font-extrabold">
-					{" "}
-					Y VIVE LA{" "}
-				</span>
+				<span className="text-[32px] font-extrabold"> Y VIVE LA </span>
 				<span className="text-pink-600 text-[32px] font-extrabold">
 					EXPERIENCIA
 				</span>
-				<span className="text-white text-[32px] font-extrabold">
-					{" "}
-					CONBOLETO{" "}
-				</span>
+				<span className="text-[32px] font-extrabold"> CONBOLETO </span>
 			</div>
 			<button className="w-[280px] h-12 rounded-lg border-2 mt-11 border-pink-600 backdrop-blur-[31.80px]">
 				CREAR EVENTO
 			</button>
+			<div className="flex justify-between w-[1074px] mt-[94px]">
+				{siteMap.map((item, index: number) => (
+					<div className="flex flex-col gap-1 relative" key={index}>
+						<p
+							className={`${roboto400.className} text-[15px] font-bold footer-main`}
+						>
+							{item.title}
+						</p>
 
-			{/* <div className="flex flex-col gap-8 relative">
-				<span className="top-[33px] absolute w-[7rem] h-[4px] bg-white opacity-10"></span>
-				<p className="text-[22px] font-bold footer-main">Our Classes</p>
-
-				<p className="text-[16px] hover:text-[#ff0366] cursor-pointer text-[#646464] font-medium hover:font-bold">
-					Fitness Classes
-				</p>
-				<p className="text-[16px] hover:text-[#ff0366] cursor-pointer text-[#646464] font-medium hover:font-bold">
-					Aerobics Classes
-				</p>
-				<p className="text-[16px] hover:text-[#ff0366] cursor-pointer text-[#646464] font-medium hover:font-bold">
-					Power Yoga
-				</p>
-				<p className="text-[16px] hover:text-[#ff0366] cursor-pointer text-[#646464] font-medium hover:font-bold">
-					Learn Machines
-				</p>
-				<p className="text-[16px] hover:text-[#ff0366] cursor-pointer text-[#646464] font-medium hover:font-bold">
-					Full-body Strength
-				</p>
+						{item.items.map((item) => (
+							<Link
+								href={item.link}
+								className={`${roboto100.className} text-[15px] hover:text-pink-500 cursor-pointer`}
+								key={item.title}
+							>
+								{item.title}
+							</Link>
+						))}
+					</div>
+				))}
 			</div>
-
-			<div className="flex flex-col gap-8 relative">
-				<p className="text-[22px] font-bold footer-main">Working Hours</p>
-
-				<span className="top-[33px] absolute w-[7rem] h-[4px] bg-[#ff0366]"></span>
-
-				<p className="text-[16px]  text-[#646464] font-bold">
-					Monday - Friday:
+			<div className="flex justify-between border-t-2 mt-[76px] w-[1074px] pt-12 border-slate-800">
+				<Image src={logo} alt="conboleto_img" className="" />
+				<p className={"text-sm leading-[14px]"}>
+					&copy; {new Date().getFullYear()} Conboleto. All Rights Reserved.
 				</p>
-				<p className="text-[16px] text-[#646464] font-medium">
-					7:00am - 21:00pm
-				</p>
-				<p className="text-[16px] text-[#646464] font-bold">Saturday:</p>
-				<p className="text-[16px] text-[#646464] font-medium">
-					7:00am - 19:00pm
-				</p>
-				<p className="text-[16px] text-[#646464] font-bold ">Sunday - Closed</p>
-			</div> */}
-			
-			<div className="border-t-2 mt-11 w-[1074px] border-slate-800">dfsfsf</div>
-            {/* <div className="w-[1074px] h-px opacity-10 bg-white" /> */}
-			{/* </div> */}
+				<div className="flex justify-end w-[145px] h-[39px] space-x-3">
+					<button className="w-[39px] h-[39px] rounded-[19.50px] border border-slate-800 flex items-center justify-center">
+						<FaFacebookF />
+					</button>
+					<button className="w-[39px] h-[39px] rounded-[19.50px] border border-slate-800 flex items-center justify-center">
+						<FaLinkedinIn />
+					</button>
+				</div>
+			</div>
 		</footer>
 	);
 };
